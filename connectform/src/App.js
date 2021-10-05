265aaaa22109beeaa964b1f1ff43dbb46af074ef
TannerB <tannerpace@gmail.com>
2021-10-05 16:28:43 -0400

    styles and input state

--- connectform/src/App.js
+++ connectform/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+  const [wordCount, setwordCount] = useState('0')
+  const [value, setvalue] = useState({})
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setvalue((oldValue) => (value))
+  }
+
+
+
+
+
   return (
     <div className="App">
-  
+      <h1>Typing Test</h1>
+
+      <textarea onChange={handleChange} />
+      <h4>Time Remaining</h4>
+      <button>Start</button>
+      <h1>{wordCount}</h1>
     </div>
   );
 }
