bf16f7aa2f8701a083d755bbcc49fdc3a40815e3
tannerpace <52814471+tannerpace@users.noreply.github.com>
2021-10-09 13:27:02 -0400

    added a timer component

--- connectform/src/App.js
+++ connectform/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import Timer from "./components/Timer";
 
 function App() {
   const [wordCount, setWordCount] = useState("0");
@@ -12,16 +13,20 @@ function App() {
 
   // run match on the input value and then get length which is how many
   const handleSubmit = () => {
+    console.log(`value`, value);
+    if (value === "") {
+      return;
+    }
     setWordCount(value.match(/(\w+)/g).length);
   };
 
   return (
     <div className="App">
       <h1>Typing Test</h1>
-      <pre>{JSON.stringify(value)}</pre>
       <textarea onChange={handleChange} />
-      <h4>Time Remaining</h4>
       <button onClick={handleSubmit}>Start</button>
+      <h1>Timer : </h1>
+      <Timer initialSeconds={5} initialMinute={4} />
       <h1>Word Count : {wordCount}</h1>
     </div>
   );
